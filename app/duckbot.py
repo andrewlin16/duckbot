51185cff2c75da068f2f250a61e99472880f11d6
Andrew Lin <andrewlin16@gmail.com>
2016-10-25 01:51:05 -0700

    Put main bot setup code inside main function

--- app/duckbot.py
+++ app/duckbot.py
@@ -19,27 +19,29 @@ def parse_arguments():
     return parser.parse_args()
 
 
-args = parse_arguments()
+def main():
+    args = parse_arguments()
 
-bot_info = config.bots[args.botname]
-CLIENT_ID = bot_info['client_id']
-TOKEN = bot_info['token']
+    bot_info = config.bots[args.botname]
+    client_id = bot_info['client_id']
+    token = bot_info['token']
+
+    bot = commands.Bot(command_prefix='/', description=_DESCRIPTION)
+
+    # Register commands to bot
+    general.register(bot)
+    emotes.register(bot)
+
+    @bot.event
+    async def on_ready():
+        print('logged in: %s (%s)' % (bot.user.name, bot.user.id))
+
+        oauth_url = discord.utils.oauth_url(
+            client_id, permissions=discord.Permissions.text())
+        print('invite me: %s' % oauth_url)
+
+    bot.run(token)
 
 
-bot = commands.Bot(command_prefix='/', description=_DESCRIPTION)
-
-# Register commands to bot
-general.register(bot)
-emotes.register(bot)
-
-
-@bot.event
-async def on_ready():
-    print('logged in: %s (%s)' % (bot.user.name, bot.user.id))
-
-    oauth_url = discord.utils.oauth_url(
-        CLIENT_ID, permissions=discord.Permissions.text())
-    print('invite me: %s' % oauth_url)
-
-
-bot.run(TOKEN)
+if __name__ == '__main__':
+    main()
