fbb3f1b52926b1de53d99da1758d00f1af39d937
Andrew Lin <andrewlin16@gmail.com>
2016-11-19 04:20:41 -0800

    Sort emotes.py cog by command name

--- app/cogs/emotes.py
+++ app/cogs/emotes.py
@@ -1,24 +1,16 @@
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-LENNY = '( ͡° ͜ʖ ͡°)'
-GIFF = '༼ つ ◕_◕ ༽つ'
 DISAPPROVAL = 'ಠ_ಠ'
 DONGER = 'ヽ༼ຈل͜ຈ༽ﾉ'
+GIFF = '༼ つ ◕_◕ ༽つ'
+LENNY = '( ͡° ͜ʖ ͡°)'
 
 
 class Emotes:
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(help=LENNY)
-    async def lenny(self):
-        await self.bot.say(LENNY)
-
-    @commands.command(help=GIFF)
-    async def giff(self):
-        await self.bot.say(GIFF)
 
     @commands.command(help=DISAPPROVAL)
     async def disapproval(self):
@@ -27,6 +19,14 @@ class Emotes:
     @commands.command(help=DONGER)
     async def donger(self):
         await self.bot.say(DONGER)
+
+    @commands.command(help=GIFF)
+    async def giff(self):
+        await self.bot.say(GIFF)
+
+    @commands.command(help=LENNY)
+    async def lenny(self):
+        await self.bot.say(LENNY)
 
 
 def setup(bot: Bot):
