249eada2fcd4db913c7d76c53a033e5748a6218f
Andrew Lin <andrewlin16@gmail.com>
2017-01-04 22:37:07 -0800

    Add support for BetterTTV emotes
    Changed a little bit of how the emotes cog works internally too to more
    easily support the different emotes API formats.

--- app/cogs/twitch_emotes.py
+++ app/cogs/twitch_emotes.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 TWITCH_EMOTES_API = 'https://twitchemotes.com/api_cache/v2/global.json'
+BTTV_EMOTES_API = 'https://api.betterttv.net/emotes'
 
 
 logger = logging.getLogger(__name__)
@@ -15,16 +16,36 @@ class TwitchEmotes:
 
     def __init__(self, bot: Bot):
         self.bot = bot
+        emote_cache = {}
 
         r = requests.get(TWITCH_EMOTES_API)
-        emote_data = r.json()
+        ttv_emote_data = r.json()
 
-        emote_template = emote_data['template']['small']
-        emote_ids = {name: info['image_id'] for name, info in
-                     emote_data['emotes'].items()}
-        emote_cache = {}
-        logger.info('Got %d emotes from Twitchemotes.com API' % len(emote_ids))
-        logger.info('Using template: %s' % emote_template)
+        ttv_emote_template = ttv_emote_data['template']['small']
+        ttv_emotes = {
+            name: (ttv_emote_template.replace(
+                '{image_id}', str(info['image_id'])), 'png')
+            for name, info in ttv_emote_data['emotes'].items()
+        }
+
+        logger.info('Got %d Twitch emotes from Twitchemotes.com API' %
+                    len(ttv_emotes))
+        logger.info('Using template: %s' % ttv_emote_template)
+
+        r = requests.get(BTTV_EMOTES_API)
+        bttv_emote_data = r.json()
+
+        bttv_emotes = {
+            data['regex']: ('https:' + data['url'], data['imageType'])
+            for data in bttv_emote_data['emotes']
+        }
+
+        logger.info('Got %d BTTV emotes from BetterTTV emotes API' %
+                    len(bttv_emotes))
+
+        emotes = {**ttv_emotes, **bttv_emotes}
+
+        logger.info('Total of %d Twitch + BTTV emotes' % len(emotes))
 
         @bot.listen('on_message')
         async def respond(message):
@@ -33,16 +54,15 @@ class TwitchEmotes:
 
             text = message.content
 
-            if text in emote_ids:
+            if text in emotes:
                 if text not in emote_cache:
-                    url = emote_template.replace('{image_id}',
-                                                 str(emote_ids[text]))
+                    url = emotes[text][0]
                     logger.info('Fetching emote %s from %s' % (text, url))
 
                     emote_img = requests.get(url).content
                     emote_cache[text] = emote_img
 
-                filename = '%s.png' % text
+                filename = '%s.%s' % (text, emotes[text][1])
                 with BytesIO(emote_cache[text]) as data:
                     await bot.send_file(message.channel, data,
                                         filename=filename)
