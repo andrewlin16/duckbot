890ca014c7b52be325570336dfd00728d981b23e
Sean Niu <seanniu93@gmail.com>
2017-04-23 12:31:53 -0700

    Fix config.example.py syntax error

--- app/config.example.py
+++ app/config.example.py
@@ -2,8 +2,8 @@
 bots = {
     '__BOT_NAME__': {
         'client_id': '__YOUR_CLIENT_ID__',
-        'token': '__YOUR_TOKEN__'
-        'cogs': ['COG_A', 'COG_B']    # Optional
+        'token': '__YOUR_TOKEN__',
+        'cogs': ['COG_A', 'COG_B']  # Optional
     }
     # Additional bots go here
 }
